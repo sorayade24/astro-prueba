f0d717fb9c0b8fb20e7b91f71ffbd2f7c1561186
sorayade <sorayadelreymartinez@gmail.com>
2024-01-27 19:17:14 +0100

    subida

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'blue': '#1fb6ff',
+			'purple': '#7e5bef',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'yellow': '#ffc82c',
+			'gray-dark': '#273444',
+			'gray': '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+			sans: ['Arial', 'sans-serif'],
+			serif: ['Times New Roman', 'serif'],
+		},
+		extend: {
+			spacing: {
+				'8xl': '96rem',
+				'9xl': '128rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+		},
+	},
+	plugins: [],
+}
